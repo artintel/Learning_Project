d2b876957b037451a761b4ab6a245e8b05d56fa8
Mayichao <3074099684@qq.com>
2020-11-30 22:02:02 +0800

    update mysql

--- MySQL/Mysql.c
+++ MySQL/Mysql.c
@@ -11,11 +11,57 @@
 #define MYC_DB_DEFAULTDB "MYC_DB"
 
 #define SQL_INSERT_TBL_USER "INSERT TBL_USER(U_NAME, U_GENGDER) VALUES('qiuxiang', 'woman');"
+#define SQL_SELECT_TBL_USER "SELECT * FROM TBL_USER;"
 
+
+#define SQL_DELETE_TBL_USER "CALL PROC_DELETE_USER('qiuxiang')"
 
 #define nullptr NULL
 
 //C U R D --> creat, update, read, delete
+
+int myc_mysql_select(MYSQL* handle){
+	// mysql_real_query --> sql
+	if(mysql_real_query(handle, SQL_SELECT_TBL_USER, strlen(SQL_SELECT_TBL_USER))){
+		printf("ysql_real_query : %s\n", mysql_error(handle));
+		return -1;
+	}
+
+	// store -->
+	MYSQL_RES* res = mysql_store_result(handle);
+	if(res == nullptr) {
+		printf("ysql_store_query : %s\n", mysql_error(handle));
+		return -2;
+	}
+	
+	// rows / fields
+
+	int rows = mysql_num_rows(res);
+	printf("rows: %d\n", rows);
+
+	int fields = mysql_num_fields(res);
+	printf("fields: %d\n", fields);
+
+	// fetch
+	MYSQL_ROW row;
+	while((row = mysql_fetch_row(res))){
+
+		int i = 0;
+		for(i = 0; i < fields; i++){
+			printf("%-10s", row[i]);
+		}
+		printf("\n");
+		
+	}
+	
+	mysql_free_result(res);
+
+	
+	return 0;
+}
+
+
+
 
 int main(){
 	MYSQL mysql;
@@ -30,13 +76,31 @@ int main(){
 	}
 
 	// mysql --> insert
-	
+	printf("case: mysql --> insert \n");
+#if 1	
 	if(mysql_real_query(&mysql, SQL_INSERT_TBL_USER, strlen(SQL_INSERT_TBL_USER))){
 		printf("ysql_real_query : %s\n", mysql_error(&mysql));
 	}
+#endif
+
+	myc_mysql_select(&mysql);
+
+	//MYSQL --> DELETE
+
+	printf("case: mysql --> delete \n");
+
+
+#if 1	
+	if(mysql_real_query(&mysql, SQL_DELETE_TBL_USER, strlen(SQL_DELETE_TBL_USER))){
+		printf("ysql_real_query : %s\n", mysql_error(&mysql));
+	}
+#endif
+
+	myc_mysql_select(&mysql);
+
+
 
 	mysql_close(&mysql);
-	printf("done\n");
 
 	return 0;
 }
